232747cc6feeabb42a63f13f0fe128f56f9999e9
Tucker Mayo & Dex Fitch <student@epicodus.com>
2015-03-02 11:38:42 -0800

    add carousel feature and css styling

--- router.js
+++ router.js
@@ -1,3 +1,7 @@
 PlumpBustle.Router.map(function() {
   this.resource('articles', {path: '/'});
+  this.resource('goat-article');
+  this.resource('hedgehog-article');
+  this.resource('monkey-article');
+  this.resource('pug-article');
 });
